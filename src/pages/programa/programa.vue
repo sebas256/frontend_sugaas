31fce4fd9fe03df99b76dd905d5b6ce512865e88
aly <alybenhur@gmail.com>
2024-09-26 18:23:54 -0500

    inicio proyecto

---
+++ src/pages/programa/programa.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>programam</h1>
+    </div>
+</template>
---
+++ src/plugins/router/routes.js
@@ -0,0 +1,63 @@
+export const routes = [
+ // { path: '/', redirect: '/dashboard' },
+  {
+    path: '/',
+    component: () => import('@/layouts/default.vue'),
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/pages/dashboard.vue'),
+      },
+      {
+        path: 'account-settings',
+        component: () => import('@/pages/account-settings.vue'),
+      },
+      {
+        path: 'typography',
+        component: () => import('@/pages/typography.vue'),
+      },
+      {
+        path: 'programa',
+        component: () => import('@/pages/programa/programa.vue'),
+      },
+      {
+        path: 'competencia',
+        component: () => import('@/pages/competencia/competencia.vue'),
+      },
+      {
+        path: 'icons',
+        component: () => import('@/pages/icons.vue'),
+      },
+      {
+        path: 'cards',
+        component: () => import('@/pages/cards.vue'),
+      },
+      {
+        path: 'tables',
+        component: () => import('@/pages/tables.vue'),
+      },
+      {
+        path: 'form-layouts',
+        component: () => import('@/pages/form-layouts.vue'),
+      },
+    ],
+  },
+  {
+    path: '/',
+    component: () => import('@/layouts/blank.vue'),
+    children: [
+      {
+        path: 'login',
+        component: () => import('@/pages/login.vue'),
+      },
+      {
+        path: 'register',
+        component: () => import('@/pages/register.vue'),
+      },
+      {
+        path: '/:pathMatch(.*)*',
+        component: () => import('@/pages/[...error].vue'),
+      },
+    ],
+  },
+]
